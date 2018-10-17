91fdf63ff5d9462c8a9f752cbfb5f4ea686c4935
Brahyan Medina <BRAHYAN_MEDINA@hotmail.com>
2018-10-16 21:01:35 -0400

    Merge branch 'master' of https://github.com/BrahyanMedina/XamarinNative
    Fixed XAML principal

---
+++ XamarinNative/XamarinNative.Common/Models/Products.cs
@@ -0,0 +1,21 @@
+﻿
+
+namespace XamarinNative.Common.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    class Products
+    {
+        [Key]
+        public int ProductId { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        public Decimal Price { get; set; }
+
+        public bool IsAvailable { get; set; }
+
+        public DateTime PublishOn { get; set; }
+    }
+}
